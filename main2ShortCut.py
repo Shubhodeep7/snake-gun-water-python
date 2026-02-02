14d4cda46aa15ee24e4231960354302071452ec8
Shubhodeep Supakar <supakar121@gmail.com>
2026-02-02 11:31:50 +0530

    Add files via upload

---
+++ main2ShortCut.py
@@ -0,0 +1,19 @@
+import random
+computer = random.choice([-1,0,1])
+youstr = input("Enter your choice: ")
+youDict = {"s" : 1, "w" : -1, "g" : 0}
+reverseDict = {1: "Snake",-1: "Water", 0: "Gun"}
+
+you = youDict[youstr]
+print(f"You chose {reverseDict[you]}\nComputer chose{reverseDict[computer]}")
+
+if(computer == you):
+    print("It's a Draw!")
+
+elif((computer - you) == -1 or  (computer - you) = 2):
+ print("You Loose!")
+
+else:
+    print("You Win!")    
+
+          #Else  -->You Win  
